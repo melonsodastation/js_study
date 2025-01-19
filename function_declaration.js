47fcd2340549eacd28e6d811f650fabe8db24479
sanhon <melonsodastation@gmail.com>
2025-01-19 22:33:09 +0900

    function study declaration

---
+++ function_declaration.js
@@ -0,0 +1,16 @@
+function sayHello() {
+    console.log("Hello world");
+}
+sayHello();
+
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+greet("KIM");
+
+function add (a ,b) {
+    return a + b;
+}
+
+const result = add(1, 3);
+console.log(`1+3 = ${result}`)
